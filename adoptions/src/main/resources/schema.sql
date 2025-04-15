aedf85cd380c7e3903da5206ce8f7e3e50346119
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-14 20:10:08 -0700

    adding all the files
    Signed-off-by: Josh Long <54473+joshlong@users.noreply.github.com>

--- adoptions/src/main/resources/schema.sql
+++ adoptions/src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-drop table dog;
+drop table if exists dog;
 create table if not exists dog
 (
     id          serial primary key,
     name        text not null,
     owner       text null,
     description text not null
-) ;
+);
