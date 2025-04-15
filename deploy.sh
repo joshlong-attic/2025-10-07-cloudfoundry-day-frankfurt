5e30b0140376f1595f19c777923f8ba9573642b5
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-14 18:00:33 -0700

    adding all the files
    Signed-off-by: Josh Long <54473+joshlong@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+ls -la pom.xml || echo "no pom.xml found" && exit 1
 ./mvnw -DskipTests clean package
+ls -al target || echo "no target directory found" && exit 1
 cf push adoptions -p target/adoptions-0.0.1-SNAPSHOT.jar
 cf logs adoptions
