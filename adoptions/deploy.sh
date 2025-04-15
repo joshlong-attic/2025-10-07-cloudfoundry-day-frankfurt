0337136f3a28924c45ce7e475f19237db793c402
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-14 17:21:12 -0700

    better cf interoperability
    Signed-off-by: Josh Long <54473+joshlong@users.noreply.github.com>

---
+++ adoptions/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+./mvnw -DskipTests clean package
+cf push adoptions -p target/adoptions-0.0.1-SNAPSHOT.jar
--- adoptions/src/main/java/com/example/web/WebApplication.java
+++ adoptions/src/main/java/com/example/web/WebApplication.java
@@ -1,11 +1,18 @@
 package com.example.web;
 
+import org.springframework.ai.model.openai.autoconfigure.OpenAiChatProperties;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
 import java.util.Map;
 
 @Controller
@@ -20,6 +27,22 @@ public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner runner (
+            OpenAiChatProperties openAiChatProperties,
+            DataSourceProperties dataSourceProperties ,
+            Environment environment) {
+        return args -> {
+            System.out.println("===============");
+            System.out.println(dataSourceProperties.determineUsername() + '/' + dataSourceProperties.determinePassword() + '/' + dataSourceProperties.determineUrl() + '/' +
+                    dataSourceProperties.determineDriverClassName());
+            System.out.println(openAiChatProperties.getCompletionsPath() +'/' + openAiChatProperties.getApiKey() + '/'+
+                    openAiChatProperties.getBaseUrl() );
+            System.out.println(environment.getProperty("VCAP_SERVICES"));
+            System.out.println(environment.getProperty("VCAP_APPLICATION"));
+        };
     }
 
 }
