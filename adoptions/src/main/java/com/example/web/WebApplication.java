492c1e431f2bee54f832f5142705b5f0aa86f0d4
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-14 17:58:11 -0700

    adding all the files
    Signed-off-by: Josh Long <54473+joshlong@users.noreply.github.com>

--- adoptions/src/main/java/com/example/web/WebApplication.java
+++ adoptions/src/main/java/com/example/web/WebApplication.java
@@ -1,23 +1,34 @@
 package com.example.web;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.ai.model.openai.autoconfigure.OpenAiChatProperties;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
 
 @Controller
 @ResponseBody
@@ -41,7 +52,7 @@ public class WebApplication {
     @Bean
     ApplicationRunner runner(
             ChatClient ai,
-            EmbeddingModel em ,
+            EmbeddingModel em,
             OpenAiChatProperties openAiChatProperties,
             DataSourceProperties dataSourceProperties,
             Environment environment) {
@@ -73,11 +84,11 @@ public class WebApplication {
                     .content();
             System.out.println("content: [" + content + "]");
             System.out.println("===============");
-            var result = em.call(new EmbeddingRequest(List.of("hello, world") ,
-                    null ));
+            var result = em.call(new EmbeddingRequest(List.of("hello, world"),
+                    null));
 
-            for (var r  : result.getResults())
-                System.out.println("embedding result: [" + Arrays.toString(r.getOutput())+ "]");
+            for (var r : result.getResults())
+                System.out.println("embedding result: [" + Arrays.toString(r.getOutput()) + "]");
 
             System.out.println("===============");
 
@@ -85,3 +96,77 @@ public class WebApplication {
     }
 
 }
+
+
+interface DogRepository extends ListCrudRepository<Dog, Integer> {
+}
+
+
+record Dog(@Id int id, String name, String owner, String description) {
+}
+
+
+@Controller
+@ResponseBody
+class SimpleAdoptionsController implements ApplicationRunner {
+
+    private final ChatClient ai;
+
+    private final Map<String, PromptChatMemoryAdvisor> memory = new ConcurrentHashMap<>();
+
+    private final Runnable vectorizeRunnable;
+
+    SimpleAdoptionsController(ChatClient.Builder ai, DogRepository repository, VectorStore vectorStore) {
+
+        this.vectorizeRunnable = () -> repository.findAll().forEach(dog -> {
+            var dogument = new Document("id: %s, name: %s, description: %s".formatted(
+                    dog.id(), dog.name(), dog.description()
+            ));
+            vectorStore.add(List.of(dogument));
+        });
+
+        var system = """
+                You are an AI powered assistant to help people adopt a dog from the adoption\s
+                agency named Pooch Palace with locations in Austin, Seoul, Tokyo, Singapore, Paris,\s
+                Mumbai, New Delhi, Barcelona, San Francisco, and London. Information about the dogs available\s
+                will be presented below. If there is no information, then return a polite response suggesting we\s
+                don't have any dogs available.
+                """;
+
+        this.ai = ai
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .defaultSystem(system)
+                .build();
+    }
+
+    @GetMapping("/{user}/inquire")
+    String inquire(@PathVariable String user, @RequestParam String question) {
+
+        var advisor = this.memory
+                .computeIfAbsent(user, u -> PromptChatMemoryAdvisor.builder(new InMemoryChatMemory())
+                .build());
+        return this.ai
+                .prompt()
+                .user(question)
+                .advisors(advisor)
+                .call()
+                .content();
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try (var ex = Executors.newVirtualThreadPerTaskExecutor()) {
+            ex.execute(this.vectorizeRunnable);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
